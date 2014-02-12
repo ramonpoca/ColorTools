875f4432999e7f5ad5602ca3318e5fae0b24ad62
Ramon Poca <ramon.poca@gmail.com>
2014-02-12 11:33:05 +0100

    Initial Commit

---
+++ Ase2Clr/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ase2Clr
+//
+//  Created by Ramon Poca on 12/02/14.
+//  Copyright (c) 2014 Ramon Poca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
