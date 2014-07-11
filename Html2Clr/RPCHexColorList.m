0693e5c83e1347498d6fb8680917a5e126525891
Ramon Poca <ramon.poca@gmail.com>
2014-07-11 16:56:13 +0200

    Improve hex color parsing

--- Html2Clr/RPCHexColorList.m
+++ Html2Clr/RPCHexColorList.m
@@ -8,6 +8,8 @@
 
 #import "RPCHexColorList.h"
 #import "NSColor+Hexadecimal.h"
+
+
 @implementation RPCHexColorList
 - (BOOL) readFromFile:(NSString *)path toColorListNamed: (NSString *) colorListName {
     self.colors = [[NSColorList alloc] initWithName:colorListName];
@@ -20,18 +22,27 @@
     NSArray *lines = [colorTxt componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
     
     for (NSString *line in lines) {
-        @try {
-            NSColor *color = [NSColor colorWithHexString:line];
-            if (color) {
-                self.colorNames = [self.colorNames arrayByAddingObject:line];
-                self.colorList = [self.colorList arrayByAddingObject:color];
-                [self.colors setColor:color forKey:line];
+        NSRegularExpression *expr = [NSRegularExpression regularExpressionWithPattern:@"\\s*([a-f0-9]{3,8})\\s*(.*)\\s*" options:NSRegularExpressionCaseInsensitive error:nil];
+        [expr enumerateMatchesInString:line options:0 range:NSMakeRange(0, line.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+            if (result.numberOfRanges == 3) {
+                NSString *hex = [line substringWithRange:[result rangeAtIndex:1]];
+                NSString *name = [line substringWithRange:[result rangeAtIndex:2]];
+                NSLog(@"Color: #%@ name:'%@'", hex, name);
+                @try {
+                    NSColor *color = [NSColor colorWithHexString:hex];
+                    if (color) {
+                        self.colorNames = [self.colorNames arrayByAddingObject:name];
+                        self.colorList = [self.colorList arrayByAddingObject:color];
+                        [self.colors setColor:color forKey:name];
+                    }
+                    
+                }
+                @catch (NSException *exception) {
+                    NSLog(@"Error: %@", exception);
+                }
             }
-            
-        }
-        @catch (NSException *exception) {
-            NSLog(@"Error: %@", exception);
-        }
+        }];
+        
     }
     return YES;
 }
